81da7ef191eee51f7483775c10282a6f21d9b3f1
Didiel Figueroa <didiel.f@gmail.com>
2021-07-12 13:37:29 -0400

    setting thunk

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,42 @@
+import fakeStoreApi from "../../apis/fakeStoreApi";
+import { ActionTypes } from "../constants/action-type";
+
+
+
+export const fetchProducts = () => {
+    return async ( dispatch ) => {
+        const response = await fakeStoreApi("/products");
+        dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: response.data });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+export const setProducts = ( products ) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products
+    }
+};
+
+export const selectedProduct = ( product ) => {
+    return {
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product
+    }
+};
+
+export const removeSelectedProduct = () => {
+    return {
+        type: ActionTypes.REMOVE_SELECTED_PRODUCT
+    };
+};
